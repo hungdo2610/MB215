dbf0dd38c6c57f7c4cae7b894b08a9408e5e1486
hungdo2610 <hungdothanh2610@gmail.com>
2025-01-16 16:08:46 -0500

    Lab2

---
+++ activity2.py
@@ -0,0 +1,4 @@
+Width = float(input('What is the width of the rectangle?'))
+Length = float(input('What is the height of the rectangle?'))
+Area = Width * Length
+print(f"The area of the rectangle is {Area} square units.")
---
+++ activity5.py
@@ -0,0 +1,5 @@
+
+PI = 3.14159
+radius = float(input('Enter the radius of the circle: '))
+area = PI * radius ** 2
+print(f'The area of the circle with radius {radius} is {area}.')
