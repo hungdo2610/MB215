ea4812ed9059da68c8706f81b34fd2f9a221d23b
hungdo2610 <hungdothanh2610@gmail.com>
2025-01-17 16:39:28 -0500

    Update activity4.py

--- activity4.py
+++ activity4.py
@@ -1,4 +1,4 @@
 Name = input('Enter name: ')
 Age = int(input('Enter age: '))
 Student_ID = int(input('Enter student ID: '))
-print(f'{Name:10}) {Age:10.2f} {Student_ID:10.2f}')
+print(f'{Name:10} {Age:10} {Student_ID:10}')
