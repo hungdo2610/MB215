2b16814a03ea53092a7613cfd7993eb7cb9bebb1
hungdo2610 <hungdothanh2610@gmail.com>
2025-02-01 20:12:15 -0500

    lab4

--- 169077028_PythonLab4/activity2part2.py
+++ 169077028_PythonLab4/activity2part2.py
@@ -10,6 +10,11 @@ def read_from_file(filename, data):
 def main():
     filename = input("Enter the filename to read from: ")
     read_from_file(filename)
+    non_existent_content = read_from_file('non_existent_file.txt')
+    if non_existent_content is None:
+        print("Could not read the non-existent file.")
+
+
 
 if __name__ == "__main__": # Call main() function if script is run directly
     main()
